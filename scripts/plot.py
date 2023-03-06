7a9f870949eae9278616b85e149cc27c1b1f9865
Håvard Brenne <hava.brenne@gmail.com>
2023-03-06 18:31:53 +0100

    fix: Update plotting

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv('example.csv')
+import matplotlib.pyplot as plt
+df = pd.read_csv('logs/test.csv')
 df['datetime'] = pd.to_datetime(df['Time'])
 
 position = df.columns.get_loc('datetime')
@@ -9,3 +10,20 @@ df["seconds"]=elapsed.dt.total_seconds()
 df.at[0, "seconds"] = 0.0
 print(np.diff(df["seconds"]))
 print("Mean update rate:", 1/np.diff(df["seconds"]).mean())
+
+title = "Test run"
+
+plt.subplot(2,1,1)
+plt.plot(df["seconds"], df["Velocity"], label="Velocity")
+plt.plot(df["seconds"], df["ControlVelocity"], label="Velocity setpoint")
+plt.ylabel("hz")
+plt.legend()
+plt.title(title)
+
+plt.subplot(2,1,2)
+plt.plot(df["seconds"], df["Torque"], label="Motor Driver Torque Nm")
+plt.ylabel("Torque [Nm]")
+plt.xlabel("Seconds")
+plt.legend()
+
+plt.show()
